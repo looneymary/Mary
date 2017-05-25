7d8eed194351242f557ef9e3314b105c904392f0
oddMary <maryjane_1992@mail.ru>
2017-05-25 14:35:51 +0300

    task 2 (доработки)

--- ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,84 +8,123 @@ using System.Collections;
 
 namespace ConsoleApp1
 {
+    class Params
+    {
+        private string filePath;
+        private ArrayList elementArr;
+        private ArrayList fullInfoArr;
+
+        public Params()
+        {
+            //Путь к файлу
+            filePath = @"D:\net\projects\looneyTypeOfCoding\ConsoleApp1\spisok.txt";
+
+            //Коллекция отдельных элементов
+            elementArr = new ArrayList();
+
+            //Коллекция строк со всей инфой для записи в файл
+            fullInfoArr = new ArrayList();
+        }
+        
+        public void addInfo()
+        {
+            Console.WriteLine("Информация о сотруднике: \n");
+
+            Console.WriteLine("Имя:");
+            string firstName = Console.ReadLine();
+            elementArr.Add(firstName);
+
+            Console.WriteLine("Фамилия:");
+            string lastName = Console.ReadLine();
+            elementArr.Add(lastName);
+
+            Console.WriteLine("Пол (м/ж):");
+            string sex = Console.ReadLine();
+            elementArr.Add(sex);
+
+            Console.WriteLine("Должность:");
+            string appointment = Console.ReadLine();
+            elementArr.Add(appointment);
+
+            Console.WriteLine("\nРабочая информация: \n");
+
+            Console.WriteLine("Дата вступления в должность:");
+            string date = Console.ReadLine();
+            elementArr.Add(date);
+
+            Console.WriteLine("Оклад:");
+            string val = Console.ReadLine();
+            int salary = int.Parse(val);
+            elementArr.Add(salary);
+        }
+        public void showAllList()
+        {
+            int arrCount = elementArr.Count;
+            int num = 1;
+            for (int j = 0; j<arrCount; j += 6)
+            {
+                //Краткая информация
+                string text = num + ". " + elementArr[j + 1] + " " + elementArr[j] + ", "
+                                         + elementArr[j + 2] + ", " + elementArr[j + 3];
+                //Полная инфа
+                string fullInfo = num + ". " + elementArr[j + 1] + " " + elementArr[j] + ", "
+                                         + elementArr[j + 2] + ", " + elementArr[j + 3] + ", "
+                                         + elementArr[j + 4] + ", " + elementArr[j + 5];
+                //Заполнение коллекции
+                fullInfoArr.Add(fullInfo);
+                num++;
+                Console.Write(text);
+                Console.WriteLine("\n");
+            }
+        }
+        public void writeToFile()
+        {
+            string textToWriteInAFile = "";
+            foreach (var a in fullInfoArr)
+            {
+                textToWriteInAFile += " " + (string)a + ";\n";
+            }
+            File.WriteAllText(filePath, textToWriteInAFile);
+        }
+        public void readFromFile()
+        {
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine(readText);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             int i;
-            //Коллекция отдельных элементов
-            ArrayList elementArr = new ArrayList();
-            //Коллекция строк со всей инфой для записи в файл
-            ArrayList fullInfoArr = new ArrayList();
-            //Путь к файлу
-            string filePath = @"D:\net\projects\looneyTypeOfCoding\ConsoleApp1\spisok.txt";
+            Params param = new Params();            
             do
             {
-                Console.Write("Меню:\n1. Ввести данные о сотруднике \n2. Вывести информацию о всех сотрудниках \n3. Запись информации в файл \n4. Чтение из файла \n5. Выйти из программы\n\n");
+                Console.Write("Меню:\n1. Ввести данные о сотруднике \n2. Вывести информацию о всех сотрудниках \n" +
+                                    "3. Запись информации в файл \n4. Чтение из файла \n5. Выйти из программы\n\n");
                 i = int.Parse(Console.ReadLine());
                 switch (i)
                 {
                     //Добавить информацию о сотруднике
                     case 1:
                         //Вынести в функцию
-                        Console.WriteLine("Информация о сотруднике: \n");
-                        Console.WriteLine("Имя:");
-                        string firstName = Console.ReadLine();
-                        elementArr.Add(firstName);
-                        Console.WriteLine("Фамилия:");
-                        string lastName = Console.ReadLine();
-                        elementArr.Add(lastName);
-                        Console.WriteLine("Пол (м/ж):");
-                        string sex = Console.ReadLine();
-                        elementArr.Add(sex);
-                        Console.WriteLine("Должность:");
-                        string appointment = Console.ReadLine();
-                        elementArr.Add(appointment);
-                        Console.WriteLine("\nРабочая информация: \n");
-                        Console.WriteLine("Дата вступления в должность:");
-                        string date = Console.ReadLine();
-                        elementArr.Add(date);
-                        Console.WriteLine("Оклад:");
-                        string val = Console.ReadLine();
-                        int salary = int.Parse(val);
-                        elementArr.Add(salary);
+                        param.addInfo();
                         break;
                     //Вывести список всех сотрудников
                     case 2:
                         //Вынести в функцию
-                        int arrCount = elementArr.Count;
-                        int num = 1;
-                        for (int j = 0; j < arrCount; j += 6)
-                        {
-                            //Краткая информация
-                            string text = num + ". " + elementArr[j + 1] + " " + elementArr[j] + ", "
-                                                             + elementArr[j + 2] + ", " + elementArr[j + 3];
-                            //Полная инфа
-                            string fullInfo = num + ". " + elementArr[j + 1] + " " + elementArr[j] + ", "
-                                                             + elementArr[j + 2] + ", " + elementArr[j + 3] + ", "
-                                                             + elementArr[j + 4] + ", " + elementArr[j + 5];
-                            //Заполнение коллекции
-                            fullInfoArr.Add(fullInfo);
-                            num++;
-                            Console.Write(text);
-                            Console.WriteLine("\n");
-                        }
+                        param.showAllList();
                         break;
                     //Запись списка в файл
                     case 3:
                         //I eta v funkciu
-                        string textToWriteInAFile = "";
-                        foreach (var a in fullInfoArr)
-                        {
-                            textToWriteInAFile += " " + (string)a + ";\n";
-                        }
-                        File.WriteAllText(filePath, textToWriteInAFile);
+                        param.writeToFile();
                         break;
                     //Чтение данных из файла
                     case 4:
                         //4itat' iz faila
-                        string readText = File.ReadAllText(filePath);
-                        Console.WriteLine(readText);
+                        param.readFromFile();
                         break;
                     case 5:
                         Console.WriteLine("Закрыть приложение");
