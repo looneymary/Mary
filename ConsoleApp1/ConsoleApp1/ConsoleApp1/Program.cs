dd57c4d36f842e5934f1a36ce00ba18b77ff1a69
oddMary <maryjane_1992@mail.ru>
2017-05-30 13:51:33 +0300

    task 2 final[3]

--- ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,95 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO; 
+using System.IO;
 using System.Collections;
 
 namespace ConsoleApp1
 {
-    class Params
-    {
-        private string filePath;
-        private ArrayList fullInfoArr;
-
-        public Params()
-        {
-            //Путь к файлу
-            filePath = @"D:\net\projects\looneyTypeOfCoding\ConsoleApp1\spisok.txt";
-
-            //Коллекция строк со всей инфой для записи в файл
-            fullInfoArr = new ArrayList();
-
-        }
-
-        public void addInfo()
-        {
-            Console.WriteLine("Информация о сотруднике: \n");
-
-            Console.WriteLine("Имя:");
-            string firstName = Console.ReadLine();
-
-            Console.WriteLine("Фамилия:");
-            string lastName = Console.ReadLine();
-
-            Console.WriteLine("Пол (м/ж):");
-            string sex = Console.ReadLine();
-
-            Console.WriteLine("Должность:");
-            string appointment = Console.ReadLine();
-
-            Console.WriteLine("\nРабочая информация: \n");
-
-            Console.WriteLine("Дата вступления в должность:");
-            string date = Console.ReadLine();
-
-            Console.WriteLine("Оклад:");
-            string val = Console.ReadLine();
-            int salary = int.Parse(val);
-
-            string fullInfo = lastName + " " + firstName + ", " + sex + ", " + appointment + ", "
-                                         + date + ", " + salary;
-            //Добавить пользователя в коллекцию
-            fullInfoArr.Add(fullInfo);
-            }
-        public void showAllList()
-        {
-            foreach (var person in fullInfoArr)
-                {
-                    Console.WriteLine(person);
-                }
-        }
-        public void showOnePerson()
-        {
-            int person;
-            Console.WriteLine("Введите порядковый номер:");
-            person = int.Parse(Console.ReadLine());
-            Console.WriteLine(fullInfoArr[person-1]);
-        }
-        public void writeToFile()
-        {
-            string textToWriteInAFile = "";
-            foreach (var a in fullInfoArr)
-            {
-                textToWriteInAFile += " " + (string)a + ";\r\n";
-            }
-            File.WriteAllText(filePath, textToWriteInAFile);
-        }
-        public void readFromFile()
-        {
-            
-            foreach (string line in File.ReadLines(filePath))
-            {
-                fullInfoArr.Add(line);
-            }
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             int i;
-            Params param = new Params();            
+            Params param = new Params();
             do
             {
                 Console.Write("Меню:\n1. Ввести данные о сотруднике \n2. Вывести информацию о всех сотрудниках \n" +
@@ -102,31 +24,31 @@ namespace ConsoleApp1
                     //Добавить информацию о сотруднике
                     case 1:
                         //Вынести в функцию
-                        param.addInfo();
+                        param.AddInfo();
 
                         break;
                     //Вывести список всех сотрудников
                     //Не придумала как пронумеровать этот список
                     case 2:
                         //Вынести в функцию
-                        param.showAllList();
+                        param.ShowAllList();
                         break;
                     //Вывести информацию об одном сотруднике
                     case 3:
                         //Вынести в функцию
-                        param.showOnePerson();
+                        param.ShowOnePerson();
                         break;
                     //Запись списка в файл
                     case 4:
                         //I eta v funkciu
-                        param.writeToFile();
+                        param.WriteToFile();
                         break;
                     //Чтение данных из файла
                     //Не нравится, что дублируются данные при выборе чтения из файла и добавления в массив, 
                     //но как это фильтровать тоже что-то не придумала
                     case 5:
                         //4itat' iz faila
-                        param.readFromFile();
+                        param.ReadFromFile();
                         break;
                     case 6:
                         Console.WriteLine("Закрыть приложение");
